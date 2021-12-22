d1c9cb576cc014eb46ceaefc16a7e654aa5d3f00
Schlauer-Hax <arzberger52@gmail.com>
2021-12-22 20:53:51 +0100

    Fixed Time

--- src/components/cards.ts
+++ src/components/cards.ts
@@ -1,9 +1,15 @@
 import { Grid, modernCard } from "@lucsoft/webgen";
 
 export const renderCards = () => {
+    let nextdate = new Date();
+    nextdate.setHours(0, 0, 0, 0);
+    nextdate.setDate(27);
+    if (nextdate.getTime() < Date.now()) {
+        nextdate.setMonth(nextdate.getMonth() + 1);
+    }
     return Grid({}, modernCard({
-        title: '10 Days',
-        description: 'Wednesday, 27.10.21',
+        title: (Math.round((nextdate.getTime() - Date.now()) / (1000 * 3600 * 24))) + ' Days',
+        description: nextdate.toLocaleDateString(),
         subtitle: 'Days until Renew'
     }))
 }
